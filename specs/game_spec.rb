5931d5ad1d9724d80820dc1d6edffb900ced5cbd
Fraser Ross <fraser182@icloud.com>
2018-12-04 10:09:54 +0000

    fixed game_spec link error

--- specs/game_spec.rb
+++ specs/game_spec.rb
@@ -9,39 +9,39 @@ class TestGame < MiniTest::Test
   end
 
   def test_check_rock_vs_scissors
-    assert_equal('rock beats scissors, so Player 1 wins!', @game1.check_win('rock', 'scissors'))
+    assert_equal('rock beats scissors, so Player 1 wins!', @game1.win_loss_or_draw('rock', 'scissors'))
   end
 
   def test_check_rock_vs_paper
-    assert_equal('paper beats rock, so Player 2 wins!', @game1.check_win('rock', 'paper'))
+    assert_equal('paper beats rock, so Player 2 wins!', @game1.win_loss_or_draw('rock', 'paper'))
   end
 
   def test_check_rock_vs_rock
-    assert_equal('It is a draw!', @game1.check_win('rock', 'rock'))
+    assert_equal('It is a draw!', @game1.win_loss_or_draw('rock', 'rock'))
   end
 
   def test_check_scissors_vs_paper
-    assert_equal('scissors beats paper, so Player 1 wins!', @game1.check_win('scissors', 'paper'))
+    assert_equal('scissors beats paper, so Player 1 wins!', @game1.win_loss_or_draw('scissors', 'paper'))
   end
 
   def test_check_scissors_vs_rock
-    assert_equal('rock beats scissors, so Player 2 wins!', @game1.check_win('scissors', 'rock'))
+    assert_equal('rock beats scissors, so Player 2 wins!', @game1.win_loss_or_draw('scissors', 'rock'))
   end
 
   def test_check_scissors_vs_scissors
-    assert_equal('It is a draw!', @game1.check_win('scissors', 'scissors'))
+    assert_equal('It is a draw!', @game1.win_loss_or_draw('scissors', 'scissors'))
   end
 
   def test_check_paper_vs_rock
-    assert_equal('paper beats rock, so Player 1 wins!', @game1.check_win('paper', 'rock'))
+    assert_equal('paper beats rock, so Player 1 wins!', @game1.win_loss_or_draw('paper', 'rock'))
   end
 
   def test_check_paper_vs_scissors
-    assert_equal('scissors beats paper, so Player 2 wins!', @game1.check_win('paper', 'scissors'))
+    assert_equal('scissors beats paper, so Player 2 wins!', @game1.win_loss_or_draw('paper', 'scissors'))
   end
 
   def test_check_paper_vs_paper
-    assert_equal('It is a draw!', @game1.check_win('paper', 'paper'))
+    assert_equal('It is a draw!', @game1.win_loss_or_draw('paper', 'paper'))
   end
 
 
